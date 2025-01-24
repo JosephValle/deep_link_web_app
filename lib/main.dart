470cd61b4587c26a388d3536cc786e5a490bb914
JosephValle <josephv2599@gmail.com>
2025-01-24 12:24:04 -0500

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Join Mycap',
+      title: 'Join Mycap!',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
