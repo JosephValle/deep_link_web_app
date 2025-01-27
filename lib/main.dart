c1bedc349796390d6f08b7bf4b7862aeb9414999
JosephValle <josephv2599@gmail.com>
2025-01-27 13:02:31 -0500

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:universal_html/html.dart' as html;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setHashUrlStrategy();
   runApp(const MyApp());
 }
 
@@ -9,26 +13,30 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Deep Link Converter',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Deep Link Converter'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Deep Link Converter',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const MyHomePage(title: 'Deep Link Converter'),
+      );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({required this.title, super.key});
 
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('title', title));
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -53,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _convertToDeepLink() {
     if (payload != null) {
-      final baseLink = "https://mycapplusbeta.page.link/";
+      const baseLink = 'https://mycapplusbeta.page.link/';
       final queryParameters = {
         'apn': 'org.vumc.mycapplusbeta',
         'isi': '6448734173',
@@ -62,53 +70,60 @@ class _MyHomePageState extends State<MyHomePage> {
       };
 
       setState(() {
-        deepLink = "$baseLink?${queryParameters.entries.map((e) => '${e.key}=${e.value}').join('&')}";
+        deepLink =
+            "$baseLink?${queryParameters.entries.map((e) => '${e.key}=${e.value}').join('&')}";
       });
     }
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (payload != null)
-                Text(
-                  "Extracted Payload:\n$payload",
-                  style: Theme.of(context).textTheme.bodyLarge,
-                  textAlign: TextAlign.center,
-                ),
-              if (deepLink != null) ...[
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (payload != null)
+                  Text(
+                    'Extracted Payload:\n$payload',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                if (deepLink != null) ...[
+                  const SizedBox(height: 20),
+                  Text(
+                    'Generated Deep Link:\n$deepLink',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      html.window.open(deepLink!, '_blank');
+                    },
+                    child: const Text('Open Deep Link'),
+                  ),
+                ],
                 const SizedBox(height: 20),
-                Text(
-                  "Generated Deep Link:\n$deepLink",
-                  style: Theme.of(context).textTheme.bodyLarge,
-                  textAlign: TextAlign.center,
-                ),
                 ElevatedButton(
-                  onPressed: () {
-                    html.window.open(deepLink!, "_blank");
-                  },
-                  child: const Text("Open Deep Link"),
+                  onPressed: _convertToDeepLink,
+                  child: const Text('Convert to Deep Link'),
                 ),
               ],
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _convertToDeepLink,
-                child: const Text("Convert to Deep Link"),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(StringProperty('payload', payload))
+      ..add(StringProperty('deepLink', deepLink));
   }
 }
