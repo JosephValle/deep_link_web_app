bdbf391eab0018530332469423e0c5eb05692b37
JosephValle <josephv2599@gmail.com>
2025-01-27 14:27:09 -0500

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,8 @@ void main() {
   final currentUrl = html.window.location.href;
   print('The current url is: $currentUrl');
 
-  // give me a test url that would work here:
 
-  
+  //https://josephvalle.github.io/deep_link_web_app/#/participantCode=123456789&endPoint=endpoint&studyCode=studycode
 
   // Parse the URL to check for 'payload' parameter
   final uri = Uri.parse(currentUrl.replaceAll('#/', ''));
@@ -30,25 +29,31 @@ class MyApp extends StatelessWidget {
   final PayloadObject? initialPayload;
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) =>
+      MaterialApp(
         // Define the initial route based on the presence of payload
         initialRoute: initialPayload != null ? '/payload' : '/',
         onGenerateRoute: (settings) {
           switch (settings.name) {
             case '/':
+              print(settings.name);
               return MaterialPageRoute(
                 builder: (context) =>
-                    const MyHomePage(title: 'Deep Link Converter'),
+                 MyHomePage(title: 'Deep Link Converter',
+                    initialPayload: initialPayload),
               );
             case '/payload':
-              return MaterialPageRoute(
-                builder: (context) => PayloadPage(
-                    title: 'Payload Handler', payload: initialPayload!),
-              );
-            default:
+              print(settings.name);
               return MaterialPageRoute(
                 builder: (context) =>
-                    const MyHomePage(title: 'Deep Link Converter'),
+                    PayloadPage(
+                        title: 'Payload Handler', payload: initialPayload!),
+              );
+            default:
+              print(settings.name);
+              return MaterialPageRoute(
+                builder: (context) =>
+                 MyHomePage(title: 'Deep Link Converter', initialPayload: initialPayload),
               );
           }
         },
@@ -69,9 +74,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
+  const MyHomePage(
+      {required this.title, required this.initialPayload, super.key});
 
   final String title;
+  final PayloadObject? initialPayload;
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -80,60 +88,29 @@ class MyHomePage extends StatefulWidget {
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(StringProperty('title', title));
+    properties.add(DiagnosticsProperty<PayloadObject?>('initialPayload', initialPayload));
   }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String? payload;
-  String? deepLink;
+
+  late PayloadObject? payload = widget.initialPayload;
 
   @override
   void initState() {
     super.initState();
-    _extractPayloadFromUrl();
   }
 
-  void _extractPayloadFromUrl() {
-    final uri = Uri.base; // Gets the current URL
-    final payloadParam = uri.queryParameters['payload'];
-    if (payloadParam != null) {
-      setState(() {
-        payload = Uri.decodeComponent(payloadParam);
-      });
-      // Optionally, navigate to the payload handler route
-      WidgetsBinding.instance.addPostFrameCallback((_) async {
-        await Navigator.pushNamed(context, '/payload', arguments: payload);
-      });
-    }
-  }
-
-  void _convertToDeepLink() {
-    if (payload != null) {
-      const baseLink = 'https://mycapplusbeta.page.link/';
-      final queryParameters = {
-        'apn': 'org.vumc.mycapplusbeta',
-        'isi': '6448734173',
-        'ibi': 'org.vumc.mycapplusbeta',
-        'link': Uri.encodeComponent(payload!),
-      };
-
-      setState(() {
-        deepLink =
-            "$baseLink?${queryParameters.entries.map((e) => '${e.key}=${e.value}').join('&')}";
-      });
-    } else {
-      // Handle the case where payload is null
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('No payload to convert.')),
-      );
-    }
-  }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) =>
+      Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
         ),
         body: Center(
           child: Padding(
@@ -143,30 +120,42 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 if (payload != null)
                   Text(
-                    'Extracted Payload:\n$payload',
-                    style: Theme.of(context).textTheme.bodyLarge,
+                    'Extracted Payload:',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .bodyLarge,
                     textAlign: TextAlign.center,
                   ),
-                if (deepLink != null) ...[
-                  const SizedBox(height: 20),
-                  Text(
-                    'Generated Deep Link:\n$deepLink',
-                    style: Theme.of(context).textTheme.bodyLarge,
-                    textAlign: TextAlign.center,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      html.window.open(deepLink!, '_blank');
-                    },
-                    child: const Text('Open Deep Link'),
-                  ),
-                ],
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _convertToDeepLink,
-                  child: const Text('Convert to Deep Link'),
+                Text(
+                  payload?.participantCode ?? 'Participant Code: N/A',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyLarge,
+                  textAlign: TextAlign.center,
                 ),
+                Text(
+                  payload?.endPoint ?? 'End Point: N/A',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  payload?.studyCode ?? 'Study Code: N/A',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+
+
               ],
+
+
             ),
           ),
         ),
@@ -175,10 +164,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties
-      ..add(StringProperty('payload', payload))
-      ..add(StringProperty('deepLink', deepLink));
+    properties.add(DiagnosticsProperty<PayloadObject?>('payload', payload));
   }
+
 }
 
 class PayloadPage extends StatelessWidget {
@@ -188,10 +176,14 @@ class PayloadPage extends StatelessWidget {
   final PayloadObject payload;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) =>
+      Scaffold(
         appBar: AppBar(
           title: Text(title),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
         ),
         body: Center(
           child: Padding(
@@ -204,17 +196,26 @@ class PayloadPage extends StatelessWidget {
                 const SizedBox(height: 10),
                 Text(
                   payload.participantCode ?? 'Participant Code: N/A',
-                  style: Theme.of(context).textTheme.bodyLarge,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyLarge,
                   textAlign: TextAlign.center,
                 ),
                 Text(
                   payload.endPoint ?? 'End Point: N/A',
-                  style: Theme.of(context).textTheme.bodyLarge,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyLarge,
                   textAlign: TextAlign.center,
                 ),
                 Text(
                   payload.studyCode ?? 'Study Code: N/A',
-                  style: Theme.of(context).textTheme.bodyLarge,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyLarge,
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(height: 20),
@@ -227,7 +228,7 @@ class PayloadPage extends StatelessWidget {
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties..add(StringProperty('title', title))
-    ..add(DiagnosticsProperty<PayloadObject>('payload', payload));
+    properties..add(StringProperty('title', title))..add(
+        DiagnosticsProperty<PayloadObject>('payload', payload));
   }
 }
