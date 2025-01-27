56e3dbe821b30125bcf4b2dc4faf49a6346983da
JosephValle <josephv2599@gmail.com>
2025-01-27 13:33:53 -0500

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'package:universal_html/html.dart' as html;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  usePathUrlStrategy();
+  // usePathUrlStrategy();
+  print('The current url is: ${html.window.location.href}');
   runApp(const MyApp());
 }
 
