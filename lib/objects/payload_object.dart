94832ca6fbf1a64ba3acde106f5e75545ea6f2ca
JosephValle <josephv2599@gmail.com>
2025-03-06 10:00:48 -0500

    Enhance PayloadObject factory; support deep link parsing and base64 payload decoding

--- lib/objects/payload_object.dart
+++ lib/objects/payload_object.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class PayloadObject {
   const PayloadObject({
     required this.participantCode,
@@ -5,19 +7,42 @@ class PayloadObject {
     required this.endPoint,
   });
 
-  factory PayloadObject.fromMap(Map<String, dynamic> map) => PayloadObject(
-        participantCode: map['participantCode'] as String?,
-        studyCode: map['studyCode'] as String?,
-        endPoint: map['endPoint'] as String?,
-      );
+  factory PayloadObject.fromMap(Map<String, dynamic> map) {
+    // Check if the map contains a 'link' parameter which holds the deep link.
+    if (map.containsKey('link')) {
+      final deepLink = Uri.parse(map['link']);
+      // Get the encoded payload from the deep link.
+      final encodedPayload = deepLink.queryParameters['payload'] ?? '';
+      // Decode the payload from base64.
+      final decodedPayload = utf8.decode(base64.decode(encodedPayload));
+      // Convert the JSON payload into a Map.
+      final Map<String, dynamic> payloadMap = jsonDecode(decodedPayload);
 
-  final String? participantCode;
-  final String? studyCode;
-  final String? endPoint;
+      return PayloadObject(
+        // The participant code is passed as a query parameter in the deep link.
+        participantCode: deepLink.queryParameters['participant'] ?? '',
+        // The study code is stored under the key 'project' in the payload.
+        studyCode: payloadMap['project'] ?? '',
+        // The endpoint is stored under the key 'endpoint' in the payload.
+        endPoint: payloadMap['endpoint'] ?? '',
+      );
+    } else {
+      // If there's no deep link, fall back to the direct query parameters.
+      return PayloadObject(
+        participantCode: map['participantCode'] as String? ?? '',
+        studyCode: map['studyCode'] as String? ?? '',
+        endPoint: map['endPoint'] as String? ?? '',
+      );
+    }
+  }
+
+  final String participantCode;
+  final String studyCode;
+  final String endPoint;
 
   Map<String, dynamic> toMap() => {
-        'participantCode': participantCode,
-        'studyCode': studyCode,
-        'endPoint': endPoint,
-      };
+    'participantCode': participantCode,
+    'studyCode': studyCode,
+    'endPoint': endPoint,
+  };
 }
