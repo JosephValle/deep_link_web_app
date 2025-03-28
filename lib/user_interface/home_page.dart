1ec44e171e00ab5d55a51711afa7d199ed6f5a5c
JosephValle <josephv2599@gmail.com>
2025-03-28 14:39:43 -0400

    Refactor snackbar display logic to always show install app message

--- lib/user_interface/home_page.dart
+++ lib/user_interface/home_page.dart
@@ -90,16 +90,14 @@ class _HomePageState extends State<HomePage> {
     final url = _createDynamicLinkUrl(payload!);
     // show a snackbar of: installApp if android
 
-    if(webOs == WebOs.android) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('installApp'.tr),
-          duration: const Duration(
-            seconds: 5,
-          ),
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('installApp'.tr),
+        duration: const Duration(
+          seconds: 5,
         ),
-      );
-    }
+      ),
+    );
     html.window.location.href = url;
   }
 
